46bee0fb84cb08093b732668ddce655ab42e58a2
achhas <51125776+achhas@users.noreply.github.com>
2020-05-02 20:16:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,63 +1,104 @@
+import numpy as np
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 
-########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='lightblue'
-color2='darkgreen'
-mytitle='Beer Comparison'
-tabtitle='beer!'
-myheading='Flying Dog Beers'
-label1='IBU'
-label2='ABV'
-githublink='https://github.com/austinlasseter/flying-dog-beers'
-sourceurl='https://www.flyingdog.com/beers/'
+    
+mapbox_access_token = "pk.eyJ1Ijoib2N0YXJ5biIsImEiOiJjazlvOXRjNHgwNml6M2xwMnpzaDZiYnVmIn0.zVZNaRlrbDGmKijUsuqXag"
 
-########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name=label2,
-    marker={'color':color2}
-)
+# example measurement stations
+lats = [41.434760, 38.436662]
+lons = [-105.925030, -88.962141]
+text = ['red', 'blue']
 
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = mytitle
-)
-
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+#viewer = jupyterlab_dash.AppViewer()
 
 
 ########### Initiate the app
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__)
 server = app.server
-app.title=tabtitle
 
-########### Set up the layout
-app.layout = html.Div(children=[
-    html.H1(myheading),
-    dcc.Graph(
-        id='flyingdog',
-        figure=beer_fig
-    ),
-    html.A('Code on Github', href=githublink),
-    html.Br(),
-    html.A('Data Source', href=sourceurl),
-    ]
-)
 
+#app = JupyterDash('SimpleExample')
+
+app.layout = html.Div([
+    # map centered to USA
+    html.Div([
+        dcc.Graph(
+            id = "mapbox",
+            figure={
+                "data": [
+                    dict(
+                        type = "scattermapbox",
+                        lat = lats,
+                        lon = lons,
+                        mode = "markers",
+                        marker = {'size': '14'},
+                        text = text
+                    )
+                ],
+                "layout": dict(
+                    autosize = True,
+                    hovermode = "closest",
+                    margin = dict(l = 0, r = 0, t = 0, b = 0),
+                    mapbox = dict(
+                        accesstoken = mapbox_access_token,
+                        bearing = 0,
+                        center = dict(lat = 38.30, lon = -90.68),
+                        style = "outdoors",
+                        pitch = 0,
+                        zoom = 3.5,
+                        layers = []
+                    )   
+                )
+            },
+            style = {"height": "100%"}
+        )
+    ], style = {"border-style": "solid", "height": "50vh"}),
+    # text container
+    html.Div([
+            html.P(id='station_id',
+                   style={'fontSize': '12px'})]),
+    # graph container
+    html.Div([
+            dcc.Graph(id='basic_graph')])
+
+])
+
+@app.callback(
+        Output('basic_graph', 'figure'),
+        [Input('mapbox', 'clickData')])
+def plot_basin(selection):
+    if selection is None:
+        return {}
+    else:
+        x_data = np.linspace(0,500,500)
+        y_data = np.random.rand(500)
+        
+        # depending on the station text use different color for line
+        if selection['points'][0]['text'] == 'red':
+            color='#ff0000'
+        else:
+            color='#0000ff'
+        data = [go.Scatter(
+                    x=x_data,
+                    y=y_data,
+                    line={'color': color},
+                    opacity=0.8,
+                    name="Graph"
+                )]
+        layout = go.Layout(
+                    xaxis={'type': 'linear', 'title': "Timestep"},
+                    yaxis={'type': 'linear', 'title': "Value"},
+                    margin={'l': 60, 'b': 40, 'r': 10, 't': 10},
+                    hovermode="closest"
+                    )
+        
+        return {'data': data, 'layout': layout}
+
+    
+#viewer.show(app)
 if __name__ == '__main__':
     app.run_server()
